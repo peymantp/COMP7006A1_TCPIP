9baf54a22b1f142c4a8481a8e903120920d3076b
peymantp <peymantparsa@gmail.com>
2017-10-01 15:56:03 -0700

    ...2

--- client.py
+++ client.py
@@ -1,37 +1,68 @@
 import socket
 import os
+from os import path
 
 def Main():
     port = 7005
 
-    clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    clientSocket = socket.socket() 
     clientSocket.connect(('',port))
 
-    filename = raw_input("Filename ->")
-    if filename != 'q':
-        clientSocket.send(filename)
-        data = clientSocket.recv(1024)
-        if data[:6] == 'Geting':
-            print data[6:]
-            filesize = long(data[6:])
-            message = raw_input("File Exists, " + str(filesize) +\
-            "Bytes, download? (Y/N)?")
-            if message == 'Y':
-                clientSocket.send('OK')
-                f = open('new_'+filename,'wb')
-                data.recv(1024)
-                totalRecv = len(data)
-                f.write(data)
-                while totalRecv < filesize:
-                    data = clientSocket.recv(1024)
-                    totalRecv += len(data)
-                    f.write(data)
-                    print "{0:.2f}".format((totalRecv/float(filesize))*100) + "% DONE"
+    while True:
+        command = raw_input("(GET/SEND)?")
+        if command == "GET":
+            clientSocket.send("GET")
 
-                print "Download Complete!"
+            listOfFiles = clientSocket.recv(1024)
+            print str(listOfFiles)
+
+            filename = raw_input("Filename ->")
+            clientSocket.send(filename)
+
+            data = clientSocket.recv(1024)
+            if data[:6] == 'Geting':
+                filesize = long(data[6:])
+                message = raw_input("File Exists, " + str(filesize) +\
+                "Bytes, download? (Y/N)?")
+                if message == 'Y':
+                    clientSocket.send('OK')
+                    f = open('client_'+filename,'w')
+                    data = clientSocket.recv(1024)
+                    totalRecv = len(data)
+                    f.write(data)
+                    while totalRecv < filesize:
+                        data = clientSocket.recv(1024)
+                        totalRecv += len(data)
+                        f.write(data)
+                        print "{0:.2f}".format((totalRecv/float(filesize))*100) + "% DONE"
+
+                    print "Download Complete!"
+                    f.close()
+            else:
+                print "File 404"
+        elif command == "SEND":
+            files = filter(path.isfile, os.listdir("./"))
+            print str(files)
+            filename = raw_input("Filename ->")
+            if path.isfile(filename):
+                clientSocket.send("SEND"+filename)
+
+                response = clientSocket.recv(1024)
+                if response == "READY":
+                    clientSocket.send( str(os.path.getsize(filename)))
+                    print "READY"
+                with open(filename,'rb') as f:
+                    while True:
+                        bytesToSend = f.read(1024)
+                        clientSocket.send(bytesToSend)
+                        if bytesToSend == "":
+                            clientSocket.send("OK")
+                            break
+            else:
+                print "No such file found"
         else:
-            print "File 404"
-    clientSocket.close()
+            print "not valid command try again"
+
 
 if __name__ == '__main__':
     Main()
--- server.py
+++ server.py
@@ -1,41 +1,61 @@
 import os
 import socket
 import threading
+from os import path
 
 
-def Get(name,sock):
-    filename = sock.recv(1024)
-    if os.path.isfile(filename):
-        sock.send("Geting " + str(os.path.getsize(filename)))
-        userResponse = sock.recv(1024)
-        if userResponse[:2] == 'OK':
-            with open(filename,'rb') as f:
-                while True:
-                    bytesToSend = f.read(1024)
-                    sock.send(bytesToSend)
-                    if bytesToSend == "":
-                        break
-    else:
-        sock.send("No such file found")
+def clientGET(name, sock):
+    command = sock.recv(1024)
+    if command[:3] == "GET":
+        files = filter(path.isfile, os.listdir("./"))
+        sock.send(str(files))
 
-    sock.close()
+        filename = sock.recv(1024)
+        if os.path.isfile(filename):
+            sock.send("Geting " + str(os.path.getsize(filename)))
+            userResponse = sock.recv(1024)
+            if userResponse[:2] == 'OK':
+                with open(filename,'rb') as f:
+                    while True:
+                        bytesToSend = f.read(1024)
+                        sock.send(bytesToSend)
+                        if bytesToSend == "":
+                            break
+        else:
+            sock.send("No such file found")
+    elif command[:4] == "SEND":
+        filename = "server_"+command[4:]
+        f = open(filename,'w')
+
+        sock.send("READY")
+        filesize = sock.recv(1024)
+        totalRecv = 0
+        while totalRecv < filesize:
+            data = sock.recv(1024)
+            totalRecv += len(data)
+            f.write(data)
+            print "{0:.2f}".format((totalRecv/float(filesize))*100) + "% DONE"
+
+        sock.send("Upload complete!")
+        f.close()
 
 def Main():
-    mySocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    mySocket = socket.socket() 
     port = 7005
     try:
         mySocket.bind(('',port))
     except socket.error as e:
         print(str(e))
-        
+
     mySocket.listen(5)
 
     print "Server Started"
     while True:
         connection, addr = mySocket.accept()
         print "client connected ip:<" + str(addr) + ">"
-        readThread = threading.Thread(target=Get, args=("retrThread",connection))
-        readThread.start()
+
+        cThread = threading.Thread(target=clientGET, args=("Thread",connection))
+        cThread.start()
 
     mySocket.close()
 
